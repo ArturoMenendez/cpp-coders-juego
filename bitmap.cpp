772d901deeacf2fa7d480f3d6c99a8193579fee1
heavy94 <heavy94@users.noreply.github.com>
2015-04-21 14:19:08 +0200

    Create bitmap.cpp

---
+++ bitmap.cpp
@@ -0,0 +1,66 @@
+//Esta clase permite abrir archivos .BMP y cargarlos como texturas para ser usadas con openGL
+
+#include "bitmap.h"
+#include "glut.h"
+#include <stdio.h>
+
+bitmap::bitmap(char *nombre_archivo){
+	int bytes_pixel;
+	long int ancho, alto, offset;
+	char buffer[3] = "\0";
+	FILE *archivo;
+	//Carga el archivo de imagen que se utilizara como textura
+	//Solo soporta BMP de 24 y 32 bits (RGB y RGBA)
+	archivo = fopen(nombre_archivo, "rb");
+	if (archivo == NULL) {
+		fclose(archivo);
+		estado = false;
+		return;
+	}
+	fread(buffer, sizeof(char), 2, archivo);
+	if ((buffer[0] == 'B') && (buffer[1] == 'M')){
+		fseek(archivo, 0x1C, SEEK_SET);
+		fread(&bytes_pixel, sizeof(int), 1, archivo);
+		bytes_pixel /= 8;
+		if ((bytes_pixel != 3) && (bytes_pixel != 4)){
+			fclose(archivo);
+			estado = false;
+			return;
+		}
+		fseek(archivo, 0x12, SEEK_SET);
+		fread(&ancho, sizeof(long int), 1, archivo);
+		fread(&alto, sizeof(long int), 1, archivo);
+		fseek(archivo, 0x0A, SEEK_SET);
+		fread(&offset, sizeof(long int), 1, archivo);
+		imagen = new unsigned char[ancho*alto*bytes_pixel];
+		fseek(archivo, offset, SEEK_SET);
+		fread(imagen, sizeof(unsigned char), ancho*alto*bytes_pixel, archivo);
+		this->ancho = ancho;
+		this->alto = alto;
+		this->bytes_pixel = bytes_pixel;
+		//Crea la textura, propiamente dicha, en openGL
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		if (bytes_pixel == 3) gluBuild2DMipmaps(GL_TEXTURE_2D, bytes_pixel, ancho, alto, GL_BGR_EXT, GL_UNSIGNED_BYTE, imagen);
+		else gluBuild2DMipmaps(GL_TEXTURE_2D, bytes_pixel, ancho, alto, GL_BGRA_EXT, GL_UNSIGNED_BYTE, imagen);
+
+		estado = true;
+	}
+	else estado = false;
+	fclose(archivo);
+}
+
+bool bitmap::verEstado(){
+	return estado;
+}
+
+void bitmap::usarTextura(){
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+}
+
+bitmap::~bitmap(){
+	if (estado) delete[] imagen;
