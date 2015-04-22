82667f14f976ddb551f95a1998a35486b8a5527e
jaimelledog <jaimelledog@gmail.com>
2015-04-22 18:22:20 +0200

    Update Vector3D.h

--- src/Vector3D.h
+++ src/Vector3D.h
@@ -1,1 +1,87 @@
+#pragma once
+class Vector3D
+{
+public:
+	float x;
+	float y;
+	float z;
+	static float prodesc(Vector3D, Vector3D);
+	static float modulo(Vector3D);
+	static Vector3D prodvect (Vector3D, Vector3D);
+	static Vector3D unitario (Vector3D);
+	static float angvect(Vector3D,Vector3D);
+	Vector3D operator+(Vector3D&);
+	Vector3D operator-(Vector3D&);
+	Vector3D operator*(float&); 
+	float operator*(Vector3D&);
+	Vector3D operator/(float&);
+	Vector3D(void);
+	~Vector3D(void);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
