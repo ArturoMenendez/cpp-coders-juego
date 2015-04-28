1e48f8045a466dd3afb64a118f846cb4c8018b25
jaimelledog <jaimelledog@gmail.com>
2015-04-29 01:25:47 +0200

    Update Esfera.h

--- src/Esfera.h
+++ src/Esfera.h
@@ -1,22 +1,25 @@
-#pragma once
-#include "Vector3D.h"
-
 class Esfera
 {
 public:
 	//variables
 	float radio;
 	Vector3D posicion;
+	Vector3D ptomira;
 	unsigned char r;
 	unsigned char g;
 	unsigned char b;
 	bool botonpulsado;
+	float angrot;
 	//metodos
 	Esfera(void);
+	~Esfera(void);
 	void Mouse(int, int, int, int);
 	void Motion(int, int);
 	void PassiveMotion(int, int);
-	~Esfera(void);
+	void Pinta();
 	void Dibuja();
+	void Rota();
+	void Mueve(unsigned char);
 };
+
 
