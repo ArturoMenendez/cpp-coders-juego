36c2dd8dfe4fb5676f1dbda0bea96d411013deb9
jaimelledog <jaimelledog@gmail.com>
2015-04-29 20:50:12 +0200

    Create Esfera.cpp

---
+++ src/Esfera.cpp
@@ -0,0 +1,21 @@
+include "Esfera.h"
+#include "glut.h"
+
+Esfera::Esfera()
+{
+	radio = 0.2;
+	posicion.x = posicion.y = posicion.z = 0;
+	g = r = 150;
+	b = 0;
+}
+
+
+Esfera::~Esfera()
+{
+}
+
+void Esfera::Dibuja(){
+	glTranslatef(posicion.x, posicion.y, posicion.z);
+	glutSolidSphere(radio,20,20);
+	glTranslatef(-posicion.x, -posicion.y, -posicion.z);
+}
