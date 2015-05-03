5fa5ba40d02fd6d72798175f1f06a57e7e3be323
jaimelledog <jaimelledog@gmail.com>
2015-05-03 20:00:42 +0200

    Create Mundo.h

---
+++ src/Mundo.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Jugador.h"
+
+class Mundo
+	
+{
+public:
+	//variables
+
+	Jugador j1;
+	
+	//metodos
+
+	void Dibuja();
+	void Pinta();
+	void Rota();
+	void Mueve(unsigned char);
+	void Anima();
+	void Mouse(int, int, int, int);
+	void Motion(int, int);
+	void PassiveMotion(int, int);
+	Mundo(void);
+	~Mundo(void);
+
+	void KeyOperations(void);
+	void NoMueve(unsigned char);
+};
+
