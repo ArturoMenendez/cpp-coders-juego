012ead6abe73ec24144400e3ff094e2620fb84f1
jaimelledog <jaimelledog@gmail.com>
2015-05-22 02:54:18 +0200

    Update Interaccion.h

--- src/Interaccion.h
+++ src/Interaccion.h
@@ -2,6 +2,7 @@
 
 #include "ListaDisparos.h"
 #include "ListaObstaculos.h"
+#include "Jugador.h"
 
 class Interaccion
 {
@@ -9,5 +10,5 @@ public:
 	Interaccion();
 	~Interaccion();
 	static void interaccion(ListaDisparos disparo, ListaObstaculos obstaculo);
+	static void interaccion(Jugador jugador, ListaObstaculos obstaculo);
 };
-
