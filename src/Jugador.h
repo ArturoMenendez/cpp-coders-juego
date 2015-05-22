bc54e79f96a929132d47503c96b18e58894305a1
jaimelledog <jaimelledog@gmail.com>
2015-05-22 02:46:30 +0200

    Update Jugador.h

--- src/Jugador.h
+++ src/Jugador.h
@@ -1,25 +1,27 @@
 #pragma once
 #include "Vector3D.h"
 #include "Pierna.h"
+#include "CrashBox.h"
+
 
 class Jugador
 {
 public:
 	//variables
-	float radio;
 	Vector3D posicion;
 	Vector3D ptomira;
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
+	CrashBox limites;
 	bool botonpulsado;
 	float angrot;
 	bool* keyStates = new bool[256];
 	Pierna p1, p2;
 	Cilindro c1;
+
 	//metodos
 	Jugador(void);
+	Jugador(Vector3D);
 	~Jugador(void);
+	CrashBox getCrashBox();
 	void Mouse(int, int, int, int);
 	void Motion(int, int);
 	void PassiveMotion(int, int);
