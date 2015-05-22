6d1b8e0b79d5d7d31de84bbc557b168f63f29232
jaimelledog <jaimelledog@gmail.com>
2015-05-22 02:49:44 +0200

    Create ListaEnemigos.h

---
+++ src/ListaEnemigos.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Enemigo.h"
+#include "Vector3D.h"
+#define MAX_ENEMIGOS 20
+
+class ListaEnemigos
+{
+public:
+	int n_enemigos;
+	Enemigo *lista[MAX_ENEMIGOS];
+public:
+	ListaEnemigos();
+	~ListaEnemigos();
+	void agregarEnemigo(Vector3D posicion, int tipo);
+	void dibujarEnemigos();
+	void actualizarEnemigos();
+	void mueveEnemigos();
+	void updateEnemigos();
+	void rotaEnemigos();
+};
+
