887f6310c3e43bbf1ac24261774ba812dc72aeb1
jaimelledog <jaimelledog@gmail.com>
2015-06-03 13:36:41 +0200

    Update CaC.cpp

--- src/CaC.cpp
+++ src/CaC.cpp
@@ -5,7 +5,7 @@ CaC::CaC() : Enemigo()
 {
 }
 
-CaC::CaC(Vector3D pos, int id) : Enemigo(pos, id)
+CaC::CaC(Vector3D pos, int id) : Enemigo(pos, id), z(0)
 {
 }
 
@@ -15,13 +15,33 @@ CaC::~CaC()
 
 void CaC::Dibuja()
 {
-
-	
-	glTranslatef(posicion.x, posicion.y, 1);
-	////////////////////
-	glutSolidTeapot(1);
-	////////////////////
-	glTranslatef(-posicion.x, -posicion.y, -1);
+	glPushMatrix();
 	lin.Dibuja();
 	limites.Dibuja();
+
+	if (z <= 0) flag = false;
+	if (z >= 0.7) flag = true;
+	
+	if (flag)
+		z-=0.02;
+	else
+		z+=0.02;
+
+	glTranslatef(posicion.x, posicion.y, 1+z);
+	glRotatef(angrot, 0, 0, 1);
+	glPushMatrix();
+	GLUquadricObj *obj = gluNewQuadric();
+	gluCylinder(obj,0.5,0.7,1.8,20,20);
+	glTranslatef(0, 0, 1.8);
+	gluDisk(obj,0,0.7,20,20);
+	glutSolidSphere(0.35,20,20);
+	
+	glPopMatrix();
+	glTranslatef(0.7, 0, 1);
+	glColor3ub(200,200,200);
+	gluDisk(obj,0,0.5,20,20);
+	gluDeleteQuadric(obj);
+	glPopMatrix();
+
+	
 }
